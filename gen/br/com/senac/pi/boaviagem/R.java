6ce797dbad2a009798ed7467f3e568aa3865e8fd
Herdeson <herdeson.1@gmail.com>
2015-05-30 11:41:35 -0300

    Aula 4 telas

--- gen/br/com/senac/pi/boaviagem/R.java
+++ gen/br/com/senac/pi/boaviagem/R.java
@@ -32,19 +32,21 @@ public final class R {
         public static final int novo_gasto=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int btnEntrar=0x7f080003;
-        public static final int configuracoes=0x7f080007;
-        public static final int imageview1=0x7f080000;
-        public static final int minhas_viagens=0x7f080006;
-        public static final int nova_viagem=0x7f080005;
-        public static final int novo_gasto=0x7f080004;
-        public static final int senha=0x7f080002;
-        public static final int usuario=0x7f080001;
+        public static final int action_settings=0x7f080009;
+        public static final int btnEntrar=0x7f080004;
+        public static final int categoria=0x7f080000;
+        public static final int configuracoes=0x7f080008;
+        public static final int imageview1=0x7f080001;
+        public static final int minhas_viagens=0x7f080007;
+        public static final int nova_viagem=0x7f080006;
+        public static final int novo_gasto=0x7f080005;
+        public static final int senha=0x7f080003;
+        public static final int usuario=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int dashboard=0x7f030001;
+        public static final int activity_gasto=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int dashboard=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -52,6 +54,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int categoria=0x7f05000b;
         public static final int configuracoes=0x7f05000a;
         public static final int entrar=0x7f050001;
         public static final int erro_login=0x7f050006;
